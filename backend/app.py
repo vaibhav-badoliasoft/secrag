d911196795c3408d080d69c2b737dba012a0dd4d
Vaibhav Saini <167213667+vaibhav-badoliasoft@users.noreply.github.com>
2026-02-04 16:01:34 -0700

    Chunking pipeline implemented

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI, UploadFile, File
+from utils.chunking import chunk_text
 from pypdf import PdfReader
+import json
 import os
 
 app = FastAPI()
+
 DATA_DIR = "../data"
 
 os.makedirs(DATA_DIR, exist_ok=True)
@@ -14,6 +17,7 @@ def health_check():
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
     contents = await file.read()
+
     temp_path = os.path.join(DATA_DIR, file.filename)
     with open(temp_path, "wb") as f:
         f.write(contents)
@@ -28,8 +32,23 @@ async def upload_file(file: UploadFile = File(...)):
     with open(text_path, "w", encoding="utf-8") as f:
         f.write(extracted_text)
 
+    chunks = chunk_text(extracted_text)
+
+    chunk_data = []
+    for index, chunk in enumerate(chunks):
+        chunk_data.append({
+        "chunk_id": index,
+        "content": chunk
+        })
+
+    chunk_filename = file.filename.replace(".pdf", "_chunks.json")
+    chunk_path = os.path.join(DATA_DIR, chunk_filename)
+    with open(chunk_path, "w", encoding="utf-8") as f:
+        json.dump(chunk_data, f, indent=2)
+
     return {
         "filename": file.filename,
-        "text_preview": extracted_text[:500],
-        "total_characters": len(extracted_text)
+        "total_characters": len(extracted_text),
+        "total_chunks": len(chunk_data),
+        "first_chunk_preview": chunk_data[0]["content"][:200] if chunk_data else ""
     }
